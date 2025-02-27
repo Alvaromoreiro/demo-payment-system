c7e8c3ecf1129657c6a2f2bf49ddb2b002d225f9
Alvaromoreiro <alvaromoreiro3@gmail.com>
2025-02-27 12:25:16 +0100

    create payment sections

--- frontend/src/components/Dashboard/Dashboard.tsx
+++ frontend/src/components/Dashboard/Dashboard.tsx
@@ -1,8 +1,27 @@
+import * as Tabs from '@radix-ui/react-tabs';
+import styles from './Dashboard.module.css';
+
 export const Dashboard = () => {
     return (
         <div>
             <h1>Dashboard</h1>
             <span>User logged in</span>
+            <Tabs.Root className={styles.TabsRoot} defaultValue="tab1">
+                <Tabs.List className={styles.TabsList} aria-label="Manage your account">
+                    <Tabs.Trigger className={styles.TabsTrigger} value="tab1">
+                        Overview
+                    </Tabs.Trigger>
+                    <Tabs.Trigger className={styles.TabsTrigger} value="tab2">
+                        Payment methods
+                    </Tabs.Trigger>
+                </Tabs.List>
+                <Tabs.Content className={styles.TabsContent} value="tab1">
+                    <div>Overview</div>
+                </Tabs.Content>
+                <Tabs.Content className={styles.TabsContent} value="tab2">
+                    <div>Payment methods</div>
+                </Tabs.Content>
+            </Tabs.Root>
         </div>
     );
 };
